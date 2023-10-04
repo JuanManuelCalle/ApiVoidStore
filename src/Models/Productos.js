f0aaec0d1a2464e499450a7b9a198cac6e7d5425
Juan Manuel <juancalle478@gmail.com>
2023-10-04 12:59:23 -0500

    Cambiando formato Productos

--- src/Models/Productos.js
+++ src/Models/Productos.js
@@ -18,7 +18,7 @@ const productoSchema = new mongoose.Schema({
         required: [true, 'La stok es required']
     },
     id_vendedor: {
-        type: String,
+        type: Number,
         required: [true, 'La stok es required']
     },
 })
