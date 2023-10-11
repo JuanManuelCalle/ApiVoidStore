4c7e5df0bb28b520a2fef136d39c14a191d586f3
Juan Manuel <juancalle478@gmail.com>
2023-10-11 12:08:13 -0500

    Agregando de vendedor

--- src/Models/User.js
+++ src/Models/User.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ['VENDEDOR', 'REGULAR'],
         default: 'REGULAR'
+    },
+    name_store: {
+        type: String,
+        default: null
     }
 })
 
