892229049dd3d213d9759b7e421f3ba65dd56faa
Juan Manuel <juancalle478@gmail.com>
2023-10-14 00:08:57 -0500

    Arreglando ruta cors

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 connect();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://voidstore.voidagencia.co/']
+    origin: ['http://localhost:3000', 'https://voidstore.voidagencia.co']
 }))
 
 auth(app);
